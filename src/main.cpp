a861d71236513af811631435a3c6cfe5a2433af2
Chipster <julien.chipster@gmail.com>
2024-07-04 20:08:09 +0200

    add find function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "fileIterator.hpp"
 #include "fileSize.hpp"
 #include "fileUtils.hpp"
+#include "find.hpp"
 #include "helloThere.hpp"
 #include "link.hpp"
 #include "listFiles.hpp"
@@ -32,14 +33,15 @@ static const struct luaL_Reg luapilot[] = {
     {"deepCopyTable", lua_deepCopyTable},
     {"fileExists", lua_fileExists},
     {"fileSize", lua_fileSize},
+    {"find", lua_find},
     {"getBasename", lua_getBasename},
     {"getExtension", lua_getExtension},
     {"getFilename", lua_getFilename},
     {"getFileSize", lua_fileSize},
-    {"link", lua_link},
-    {"listFiles", lua_listFiles},
     {"getMemoryUsage", lua_getMemoryUsage},
     {"getPath", lua_getPath},
+    {"link", lua_link},
+    {"listFiles", lua_listFiles},
     {"helloThere", lua_helloThere},
     {"mergeTables", lua_mergeTables},
     {"mkdir", lua_mkdir},
